49a65e26261382ebbf9b332437814a4f5733c497
VarMonke <var.monke@gmail.com>
2022-07-04 18:32:37 +0530

    Nice

--- src/project-info.js
+++ src/project-info.js
@@ -1,5 +1,5 @@
 // Make the DIV element draggable:
-dragElement(document.getElementById("project-info"));
+dragElement(document.getElementById("you-see"));
 
 function dragElement(elmnt) {
   var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
@@ -30,6 +30,21 @@ function dragElement(elmnt) {
     pos2 = pos4 - e.clientY;
     pos3 = e.clientX;
     pos4 = e.clientY;
+    console.log(pos1, pos2);
+    //console.log(pos3, pos4);
+    // if the element is out of the window, put it in the edge:
+    if (elmnt.offsetTop <= 5) {
+      elmnt.style.top = 5 + "px";
+    }
+    if (elmnt.offsetLeft <= 5) {
+      elmnt.style.left = 5 + "px";
+    }
+    if (elmnt.offsetTop >= window.innerHeight - elmnt.offsetHeight - 5) {
+      elmnt.style.top = window.innerHeight - elmnt.offsetHeight - 5 + "px";
+    }
+    if (elmnt.offsetLeft >= window.innerWidth - elmnt.offsetWidth - 5) {
+      elmnt.style.left = window.innerWidth - elmnt.offsetWidth - 5 + "px";
+    }
     // set the element's new position:
     elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
     elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
